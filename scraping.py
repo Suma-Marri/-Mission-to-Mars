9c8c80907d18c19ad41f10eb623b2d26dc350bc3
Suma Marri <smarri0612@gmail.com>
2022-03-02 16:57:57 -0500

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,31 +1,33 @@
 
-# Import Splinter and BeautifulSoup
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-import pandas as pd
-from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd 
+import datetime as dt 
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com/'
-browser.visit(url)
+def scrape_all():
+    # Initiate headless driver
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    # Set executable path and initialize the chrome browser in splinter 
+    #executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+    #browser = Browser('chrome', **executable_path)
 
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # Since these are pairs 
+    news_title, news_paragraph= mars_news(browser)
+    hemisphere_image_urls=hemisphere(browser)
+    # Run all scraping functions and store results in dictionary 
+    data={
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "hemispheres": hemisphere_image_urls,
+        "last_modified": dt.datetime.now()
+    }
 
-# Convert the browser html to a soup object and then quit the browser
-html = browser.html
-news_soup = soup(html, 'html.parser')
-
-slide_elem = news_soup.select_one('div.list_text')
-slide_elem.find('div', class_='content_title')
-
-# Use the parent element to find the first 'a' tag and save it as 'news_title'
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
-
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 def mars_news(browser):
 
@@ -117,7 +119,42 @@ def mars_facts():
     df.columns=['description', 'value']
     df.set_index('description', inplace=True)
 
+    # Convert dataframe into HTML format, add bootstrap
     #Convert back to HTML format, add bootstrap
     return df.to_html()
 
-browser.quit()
+
+# ## Scrape Hemisphere
+
+def hemisphere(browser):
+    url='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+
+    hemisphere_image_urls = []
+
+    imgs_links= browser.find_by_css("a.product-item h3")
+
+    for x in range(len(imgs_links)):
+        hemisphere={}
+
+        # Find elements going to click link 
+        browser.find_by_css("a.product-item h3")[x].click()
+
+        # Find sample Image link
+        sample_img= browser.find_link_by_text("Sample").first
+        hemisphere['img_url']=sample_img['href']
+
+        # Get hemisphere Title
+        hemisphere['title']=browser.find_by_css("h2.title").text
+
+        #Add Objects to hemisphere_img_urls list
+        hemisphere_image_urls.append(hemisphere)
+
+        # Go Back
+        browser.back()
+    return hemisphere_image_urls
+
+if __name__== "__main__":
+    # If running as script, print scrapped data
+    print(scrape_all())
