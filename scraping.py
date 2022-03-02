6f53320664bf4142919acb9ca11b14aa33881910
Suma Marri <smarri0612@gmail.com>
2022-03-02 16:41:42 -0500

    Updated scraping.py

--- scraping.py
+++ scraping.py
@@ -5,12 +5,8 @@ from bs4 import BeautifulSoup as soup
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
 # Visit the mars nasa news site
-url = 'https://redplanetscience.com'
+url = 'https://redplanetscience.com/'
 browser.visit(url)
 
 # Optional delay for loading the page
@@ -21,10 +17,9 @@ html = browser.html
 news_soup = soup(html, 'html.parser')
 
 slide_elem = news_soup.select_one('div.list_text')
-
 slide_elem.find('div', class_='content_title')
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
+# Use the parent element to find the first 'a' tag and save it as 'news_title'
 news_title = slide_elem.find('div', class_='content_title').get_text()
 news_title
 
@@ -32,27 +27,78 @@ news_title
 news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 news_p
 
+def mars_news(browser):
+
+    # visit NASA website 
+    url= 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+
+    #Optional delay for website 
+    # Here we are searching for elements with a specific combination of tag (ul) and (li) and attriobute (item_lit) and (slide)
+    # Ex. being <ul class= "item_list">
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+    # HTML Parser. Convert the brpwser html to a soup object and then quit the browser
+    html= browser.html 
+    news_soup= soup(html, 'html.parser')
+
+    # Add try/except for error handling
+    try:
+        #slide_elem looks for <ul /> tags and descendents <li />
+        # the period(.) is used for selecting classes such as item_list
+        slide_elem= news_soup.select_one('ul.item_list li.slide')
+
+        # Chained the (.find) to slide_elem which says this variable holds lots of info, so look inside to find this specific entity
+        # Get Title
+        news_title=slide_elem.find('div', class_= 'content_title').get_text()
+        # Get article body
+        news_p= slide_elem.find('div', class_='article_teaser_body').get_text()
+
+    except AttributeError:
+        return None,None
+
+    return news_title, news_p
+
 # ## Images
 
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+def featured_image(browser):
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Visit URL 
+    url= 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mar'
+    browser.visit(url)
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Find and click the full_image button
+    full_image_elem= browser.find_by_id('full_image')[0]
+    full_image_elem.click()
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+    # Find the more info button and click that 
+    # is_element_present_by_text() method to search for an element that has the provided text
+    browser.is_element_present_by_text('more info', wait_time=1)
 
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    # will take our string 'more info' and add link associated with it, then click
+    more_info_elem=browser.links.find_by_partial_text('more info')
+    more_info_elem.click()
+
+    # Parse the resulting html with soup
+    html=browser.html
+    img_soup=soup(html, 'html.parser')
+
+    # Add try/except for error handling
+    try:
+        # Find the relative image url 
+        # The 'figure.lede' references the <figure /> tag and its class=lede
+        # the 'a' is the next tag nested inside the <figure /> tag, as well as the 'img' tag 
+        # the .get('src') pulls the link to the image
+
+        # WE are telling soup to go to figure tag, then within that look for an 'a' tag then within that look for a 'img' tag
+        img_url_rel= img_soup.select_one('figure.lede a img').get("src")
+    
+    except AttributeError:
+        return None
+    # Need to get the FULL URL: Only had relative path before
+    img_url= f'https://www.jpl.nasa.gov{img_url_rel}'
+
+    return img_url
 
 # ### Mars Facts
 
